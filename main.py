22ff1e2328ca53f247242caeb1a52ee76c90d84f
Filipa Correia <filipaincorreia@gmail.com>
2021-08-13 15:58:36 +0100

    Add agent

--- main.py
+++ main.py
@@ -1,103 +1,63 @@
-class WorldObject():
-	def __init__(self, name):
-		self.Name = name
+from GridWorld import *
+from random import random, choice, randrange
 
-	def SetPos(self, x, y):
-		self.PosX = x
-		self.PosY = y
+class RLAgent():
+	def __init__(self, numStates, actions, gamma, stepSize, epsilon, planningStep):
+		self.Gamma = gamma
+		self.StepSize = stepSize
+		self.Epsilon = epsilon
+		self.PlanningStep = planningStep
+		self.PastAction = -1
+		self.PastState = -1
+		self.Model = {}
 
-class Door(WorldObject):
-	def __init__(self, name):
-		WorldObject.__init__(self, name)
-		self.IsOpen = False
+		self.Actions = {}
+		for i in range(len(actions)):
+			self.Actions[i] = actions[i]
 
-class GoalButton(WorldObject):
-	def __init__(self, name):
-		WorldObject.__init__(self, name)
-		self.WasPressed = False
-
-class DoorButton(GoalButton):
-	def __init__(self, name, door):
-		WorldObject.__init__(self, name)
-		self.Door = door
-
-class Agent(WorldObject):
-	def __init__(self, name, x, y):
-		WorldObject.__init__(self, name)
-		self.InitialPosX = x
-		self.InitialPosY = y
-
-class GridWorld:
-	def __init__(self, grid):
-		self.grid = grid
+		self.QTable = []
+		for st in range(numStates):
+			line = []
+			for a in range(len(actions)):
+				line.append(0)
+			self.QTable.append(line)
 
 
-def CreateGridWorld(filename):
-	file = open(filename)
-	lines = file.readlines()
-	startReadingGrid = False
-	doorButtons = {}
-	goalButtons = {}
-	doors = {}
-	agents = {}
-	grid =[]
+	def UpdateModel(self, prevState, action, newState, reward):
+		if not prevState in self.Model:
+			self.Model[prevState] = {}
+		if not action in self.Model[prevState]:
+			self.Model[prevState][action] = {}
+		self.Model[prevState][action] = (newState, reward)
 
-	rowNum = 0
-	colNum = 0
-	for line in lines:
-		if line[-1] == "\n":
-			line = line[:-1] #remote \n
-		cells = line.split(",")
 
-		if not startReadingGrid and line[0] == "|":
-			startReadingGrid = True
-			cells[0] = cells[0][1:] #remote |
-		
-		if startReadingGrid:
-			row = []
-			for cell in cells:
-				if cell.startswith("w"):
-					row.append("wa")
-				elif cell.startswith("f"):
-					row.append("fr")
-				else:
-					print "Unknown cell: " + str(rowNum) + " " + str(colNum)
-
-				if "cb" in cell:
-					buttonName = cell[cell.index("button"):]
-					goalButtons[buttonName].SetPos(rowNum, colNum)
-				elif "b" in cell:
-					buttonName = cell[cell.index("button"):]
-					doorButtons[buttonName].SetPos(rowNum, colNum)
-				elif "d" in cell:
-					doorName = cell[cell.index("door"):]
-					doors[doorName].SetPos(rowNum, colNum)
-				elif "a" in cell:
-					agentName = cell[cell.index("agent"):]
-					agent = Agent(agentName, rowNum, colNum)
-					agents[agentName] = agent
-
-				colNum += 1
-			colNum = 0
-			rowNum += 1
-
-			grid.append(row)
+	def ChooseActionEgreedy(self, state):
+		if random() < self.Epsilon:
+			return randrange(len(self.Actions.keys()))
 		else:
-			if cells[0] != "" and cells[1] != "":
-				buttonName = cells[0]
-				doorName = cells[1]
-				door = Door(doorName)
-				button = DoorButton(buttonName, door)
-				doors[doorName] = door
-				doorButtons[buttonName] = button
-			elif cells[0] != "":
-				buttonName = cells[0]
-				button = GoalButton(buttonName)
-				goalButtons[buttonName] = button
-			else:
-				print "Unknow init line: " + line
+			qValues = self.QTable[state]
+			top = float("-inf")
+			ties = []
+
+			for i in range(len(qValues)):
+				if qValues[i] > top:
+					top = qValues[i]
+					ties = []
+				elif qValues[i] == top:
+					ties.append(i)
+
+    		return choice(ties)
+
+#def runDynaQ(self, gridWorld, agent):
 
 
 
 if __name__ == "__main__":
-    CreateGridWorld("scenarios/scenario1.txt")
+	gridWorld = CreateGridWorld("scenarios/scenario1.txt")
+	gridWorld.Reset()
+
+	agent = RLAgent(4, ["UP", "DOWN", "LEFT", "RIGHT", "PRESS", "NOTHING"], 0.95, 0.1, 0.1, 10)
+	action = agent.ChooseActionEgreedy(0)
+	print agent.Actions[action]
+
+
