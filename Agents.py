d53a2b943f8c21cdbc325ce47c9bdf4563fd4505
PipzCorreiaz <filipaincorreia@gmail.com>
2021-08-20 16:40:09 +0100

    Reorganize files.

---
+++ Agents.py
@@ -0,0 +1,143 @@
+from GridWorld import *
+from random import random, choice, randrange
+
+
+class Agent(WorldObject):
+	def __init__(self, name, x, y):
+		WorldObject.__init__(self, name)
+		self.InitialPosX = x
+		self.InitialPosY = y
+
+	def Reset(self):
+		self.SetPos(self.InitialPosX, self.InitialPosY)
+
+
+class RLAgent(WorldObject):
+	def __init__(self, name, actions, stepSize, discount, epsilon, planningStep):
+		WorldObject.__init__(self, name)
+		self.StepSize = stepSize
+		self.Discount = discount
+		self.Epsilon = epsilon
+		self.PlanningStep = planningStep
+		self.Model = {}
+		self.QTable = {}
+		self.Actions = {}
+		for i in range(len(actions)):
+			self.Actions[i] = actions[i]
+
+
+	def Reset(self):
+		self.PosX = self.InitialPosX
+		self.PosY = self.InitialPosY
+
+
+	def SetInitialPosition(self, x, y):
+		self.InitialPosX = x
+		self.InitialPosY = y
+
+
+	#it assumes each coordinate of the position is below 99
+	def GetState(self, env):
+		state = ""
+		if self.PosX < 10:
+			state += "0"
+		state += str(self.PosX)
+		if self.PosY < 10:
+			state += "0"
+		state += str(self.PosY)
+		for b in env.DoorButtons.values():
+			if b.WasPressed:
+				state += "1"
+			else:
+				state += "0"
+		for b in env.GoalButtons.values():
+			if b.WasPressed:
+				state += "1"
+			else:
+				state += "0"
+		return state
+
+
+	def Act(self, state):
+		return self.ChooseCurrentBestAction(state)
+
+
+	def Eval(self, env):
+		env.Reset()
+		step = 0
+		done = False
+		while step < 60 and not done:
+			step += 1
+			state = self.GetState(env)
+			action = self.Act(state)
+			_, done = env.Step(self.Name, action)
+			env.Render()
+		print("Total Steps: " + str(step))
+
+
+
+	def UpdateModel(self, prevState, action, nextState, reward):
+		if not prevState in self.Model:
+			self.Model[prevState] = {}
+		self.Model[prevState][action] = (nextState, reward) #will I do this regadless of having done already?
+
+
+	def UpdateQTable(self, prevState, action, nextState, reward):
+		if not prevState in self.QTable:
+			self.QTable[prevState] = []
+			for a in self.Actions:
+				self.QTable[prevState].append(0)
+
+
+		nextBestAction = self.ChooseCurrentBestAction(nextState)
+		nextBestQvalue = self.QTable[nextState][nextBestAction]
+		self.QTable[prevState][action] += self.StepSize * (reward + self.Discount * nextBestQvalue - self.QTable[prevState][action])
+
+
+	def ChooseCurrentBestAction(self, state):
+		if not state in self.QTable:
+			self.QTable[state] = []
+			for a in self.Actions.keys():
+				self.QTable[state].append(0)
+		qValues = self.QTable[state]
+		top = float("-inf")
+		ties = []
+
+		for i in range(len(qValues)):
+			if qValues[i] > top:
+				top = qValues[i]
+				ties = [i]
+			elif qValues[i] == top:
+				ties.append(i)
+		return choice(ties)
+
+
+	def ChooseActionEgreedy(self, state):
+		if random() < self.Epsilon:
+			return randrange(len(self.Actions.keys()))
+		else:
+			return self.ChooseCurrentBestAction(state)
+
+
+	#using dynaQ
+	def Learn(self, env):
+		for i in range(300):
+			step = 0
+			done = False
+			env.Reset()
+
+			while step < 1000 and not done:
+				step += 1
+				prevState = self.GetState(env)
+				action = self.ChooseActionEgreedy(prevState)
+				reward, done = env.Step(self.Name, action)
+				nextState = self.GetState(env)
+				self.UpdateQTable(prevState, action, nextState, reward)
+				self.UpdateModel(prevState, action, nextState, reward)
+
+				for j in range(self.PlanningStep):
+					randomState = choice(list(self.Model.keys()))
+					randomAction = choice(list(self.Model[randomState].keys()))
+					predictedState, predictedReward = self.Model[randomState][randomAction]
+					self.UpdateQTable(randomState, randomAction, predictedState, predictedReward)
+			print("Episode: " + str(i) + " Steps: " + str(step))
--- GridWorld.py
+++ GridWorld.py
@@ -42,16 +42,6 @@ class DoorButton(GoalButton):
 		self.Door.IsOpen = True
 
 
-class Agent(WorldObject):
-	def __init__(self, name, x, y):
-		WorldObject.__init__(self, name)
-		self.InitialPosX = x
-		self.InitialPosY = y
-
-	def Reset(self):
-		self.SetPos(self.InitialPosX, self.InitialPosY)
-
-
 class GridWorld:
 	def __init__(self, grid, agents, doors, doorButtons, goalButtons):
 		self.Grid = grid #array of arrays
@@ -180,72 +170,3 @@ class GridWorld:
 			print(printableRow)
 
 
-def CreateGridWorld(filename, a0):
-	file = open(filename)
-	lines = file.readlines()
-	startReadingGrid = False
-	doorButtons = {}
-	goalButtons = {}
-	doors = {}
-	agents = {}
-	grid =[]
-
-	rowNum = 0
-	colNum = 0
-	for line in lines:
-		if line[-1] == "\n":
-			line = line[:-1] #remote \n
-		cells = line.split(",")
-
-		if not startReadingGrid and line[0] == "|":
-			startReadingGrid = True
-			cells[0] = cells[0][1:] #remote |
-		
-		if startReadingGrid:
-			row = []
-			for cell in cells:
-				if cell.startswith("w"):
-					row.append("w")
-				elif cell.startswith("f"):
-					row.append("f")
-				else:
-					print("Unknown cell: " + str(rowNum) + " " + str(colNum))
-
-				if "cb" in cell:
-					buttonName = cell[cell.index("button"):]
-					goalButtons[buttonName].SetPos(rowNum, colNum)
-				elif "b" in cell:
-					buttonName = cell[cell.index("button"):]
-					doorButtons[buttonName].SetPos(rowNum, colNum)
-				elif "d" in cell:
-					doorName = cell[cell.index("door"):]
-					doors[doorName].SetPos(rowNum, colNum)
-				elif "a" in cell:
-					agentName = cell[cell.index("agent"):]
-					if agentName == a0.Name:
-						agents[agentName] = a0
-						a0.SetInitialPosition(rowNum, colNum)
-					else:
-						agent = Agent(agentName, rowNum, colNum)
-						agents[agentName] = agent
-
-				colNum += 1
-			colNum = 0
-			rowNum += 1
-			grid.append(row)
-		else:
-			if cells[0] != "" and cells[1] != "":
-				buttonName = cells[0]
-				doorName = cells[1]
-				door = Door(doorName)
-				button = DoorButton(buttonName, door)
-				doors[doorName] = door
-				doorButtons[buttonName] = button
-			elif cells[0] != "":
-				buttonName = cells[0]
-				button = GoalButton(buttonName)
-				goalButtons[buttonName] = button
-			else:
-				print("Unknow init line: " + line)
-	
-	return GridWorld(grid, agents, doors, doorButtons, goalButtons)
